f7ac1141218f69288cd961d3631ba5d4af4535c5
hu <yzouji@gmail.com>
2022-10-12 17:09:49 +0800

    deploy. add OrderBookReader

---
+++ deploy/OrderBookReader.ts
@@ -0,0 +1,27 @@
+import { DeployFunction } from 'hardhat-deploy/types'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+
+const deployFunction: DeployFunction = async function ({ deployments, getNamedAccounts }: HardhatRuntimeEnvironment) {
+  console.log('Running OrderBookReader deploy script')
+  const { deploy } = deployments
+
+  const { deployer } = await getNamedAccounts()
+  // console.log('Deployer:', deployer)
+
+  const { address } = await deploy('OrderBookReader', {
+    from: deployer,
+    log: true,
+    deterministicDeployment: false,
+    skipIfAlreadyDeployed: false,
+    // waitConfirmations: 3,
+    args: [],
+  })
+
+  console.log('OrderBookReader deployed at ', address)
+}
+
+export default deployFunction
+
+deployFunction.dependencies = []
+
+deployFunction.tags = ['OrderBookReader']
