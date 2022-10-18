670c8a4eeeef47ca4f1f6e4403a3d5eedf8300a6
hu <yzouji@gmail.com>
2022-10-18 17:41:55 +0800

    deploy. add Reader

---
+++ deploy/Reader.ts
@@ -0,0 +1,27 @@
+import { DeployFunction } from 'hardhat-deploy/types'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+
+const deployFunction: DeployFunction = async function ({ deployments, getNamedAccounts }: HardhatRuntimeEnvironment) {
+  console.log('Running Reader deploy script')
+  const { deploy } = deployments
+
+  const { deployer } = await getNamedAccounts()
+  // console.log('Deployer:', deployer)
+
+  const { address } = await deploy('Reader', {
+    from: deployer,
+    log: true,
+    deterministicDeployment: false,
+    skipIfAlreadyDeployed: false,
+    // waitConfirmations: 3,
+    args: [],
+  })
+
+  console.log('Reader deployed at ', address)
+}
+
+export default deployFunction
+
+deployFunction.dependencies = []
+
+deployFunction.tags = ['Reader']
